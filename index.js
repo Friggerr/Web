9ab08dfa6f40d5874ca5ffd37e6b3f3d16cfe82b
friggerr <sarun.ec9@gmail.com>
2016-11-13 22:29:55 +0700

    deploy on server

--- index.js
+++ index.js
@@ -7,15 +7,60 @@ var helloService = {
     Hello_Service: {
         Hello_Port: {
             sayHello: function(args) {
-                  var xml = require('fs').readFileSync('movieG5.xml', 'utf8')
-                  var doc = new dom().parseFromString(xml)
-                  var nodes = xpath.select("/movielist/movie/name/text()", doc).toString()
-                  console.log(nodes)
-                  return {
-                      title:nodes
-                  };
+              var xml = require('fs').readFileSync('movieG5.xml', 'utf8')
+              xml = xml.replace(/(\r\n|\n|\r|\t)/gm, "");
+              var doc = new dom().parseFromString(xml)
+              var nodes = xpath.select("/movielist", doc);                  //console.log(xmlDoc[0].localName)
+              var newMovie = doc.createElement("movie")
+
+              name = doc.createElement("name");
+              txtName = doc.createTextNode(args.Movie)
+              name.appendChild(txtName);
+
+              director = doc.createElement("director");
+              txtDir = doc.createTextNode(args.Director)
+              director.appendChild(txtDir );
+
+              year = doc.createElement("year");
+              txtYear =doc.createTextNode(args.Year)
+              year.appendChild(txtYear );
+ 
+              genres = doc.createElement("genres");
+              genre = doc.createElement("genre");
+              txtGenre=doc.createTextNode(args.genre[1])
+              genre.appendChild(txtGenre);
+              genres.appendChild(genre);
+
+              stars = doc.createElement("stars");
+              stName = doc.createElement("name");
+              txtStr=doc.createTextNode(args.name)
+              stName.appendChild(txtStr);
+              stars.appendChild(stName);
+
+              newMovie = doc.createElement("movie");
+              newMovie.appendChild(name);
+              newMovie.appendChild(director);
+              newMovie.appendChild(year);
+              newMovie.appendChild(genres);
+              newMovie.appendChild(stars);
+              doc.getElementsByTagName("movielist")[0].appendChild(newMovie);
+              var result = nodes.toString();
+              return {xml:result};
+
               
-            }
+            },
+            sayHello2: function(args) {
+                  var xml = require('fs').readFileSync('movieG5.xml', 'utf8')
+                  xml = xml.replace(/(\r\n|\n|\r|\t)/gm, "");
+                  var doc = new dom().parseFromString(xml)
+                  var nodes = xpath.select("/movielist", doc);
+                  console.log(nodes[0].getElementsByTagName("movie").length);
+                  for (var i = 0; i < nodes[0].getElementsByTagName("movie").length; i++) {
+                      nodes[0].getElementsByTagName("movie")[i].getElementsByTagName("name")[0].childNodes[0].data = "TESTCHANGEMOVIENAME1";
+                  }
+                  console.log(nodes[0].getElementsByTagName("movie")[0].getElementsByTagName("name")[0].toString());
+                  return { xml: nodes.toString() };
+                }
         }
     }
 }
@@ -24,7 +69,7 @@ var xml = require('fs').readFileSync('HelloService.wsdl', 'utf8'),
           response.end("404: Not Found: " + request.url)
       });
 
-server.listen( process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen( 8000,"127.0.0.1", function(){
   var addr = server.address();
   console.log("server listening at", addr.address + ":" + addr.port);
   });
--- soap_client.js
+++ soap_client.js
@@ -1,6 +1,15 @@
 var soap = require('soap');
   var url = 'https://st-soap.herokuapp.com/wsdl?wsdl';
-  var args = {name: "12"};
+  var args = {
+              function:"edit",
+              path: "/movielist",
+              value : "Exam",
+              Movie: "Eaxammm",
+              Director: "Director",
+              Year : "1222",
+              genre : "dama",
+              name : "harry"
+            };
   soap.createClient(url, function(err, client) {
       client.sayHello(args, function(err, result) {
           console.log(result);
